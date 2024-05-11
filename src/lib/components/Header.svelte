87be81ea16fcfc23a7bb3b09b55657669039de01
jbellny <62436650+jbellny@users.noreply.github.com>
2024-05-11 10:09:57 -0400

    implement header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import SearchBar from './SearchBar.svelte';
+</script>
+
+<header
+	class="sticky left-0 top-0 z-10 flex h-[64px] items-center justify-between bg-white px-4 py-2"
+>
+	<div class="flex items-center gap-4">
+		<img
+			src="/icons/burgerIcon.svg"
+			class="cursor-pointer rounded-full p-3 hover:bg-gray-200"
+			alt=""
+		/>
+		<img src="/icons/ytLogo.svg" alt="" />
+	</div>
+	<SearchBar />
+	<div class="flex items-center gap-3">
+		<img
+			src="/icons/createIcon.svg"
+			class="cursor-pointer rounded-full p-3 hover:bg-gray-200"
+			alt=""
+		/>
+		<img
+			src="/icons/noteIcon.svg"
+			class="cursor-pointer rounded-full p-3 hover:bg-gray-200"
+			alt=""
+		/>
+		<div class="flex h-12 w-12 items-center justify-center rounded-full hover:bg-gray-200">
+			<img src="/avatar.png" class="h-8 w-8 cursor-pointer rounded-full hover:bg-gray-200" alt="" />
+		</div>
+	</div>
+</header>
---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,23 @@
+<form action="" class="flex basis-[400px]">
+	<input
+		class="h-10 flex-1 rounded-s-3xl border border-gray-400 px-3 outline-none focus:border-2 focus:border-blue-600"
+		type="text"
+	/>
+	<button class="flex w-16 items-center justify-center">
+		<img src="/icons/searchIcon.svg" alt="" />
+	</button>
+</form>
+
+<!-- <form method="POST" class="flex h-[40px] basis-[600px]">
+	<input
+		type="text"
+		class="h-[40px] flex-1 rounded-s-3xl border border-gray-400 px-3 outline-none focus:border-2 focus:border-blue-600"
+		placeholder="Search"
+		name="q"
+	/>
+	<button
+		class="flex w-16 items-center justify-center rounded-e-3xl border border-l-0 border-gray-400 bg-slate-50"
+	>
+		<img src="/icons/searchIcon.svg" alt="" />
+	</button>
+</form> -->
